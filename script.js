a099642cd8c550ec96e84afc93599be16dbea97a
Dibosh Sarker <diboshsarker45@gmail.com>
2026-02-13 04:26:50 +0600

    Nothing new

--- script.js
+++ script.js
@@ -181,21 +181,22 @@
 //   console.log(line);
 // }
 
-for(let i = 1; i <= 5; i++){
-  let line = "";
+// for(let i = 1; i <= 5; i++){
+//   let line = "";
   
-  for(let s = 1; s <= 5 -i; s++){
-    line += " ";
-  }
+//   for(let s = 1; s <= 5 -i; s++){
+//     line += " ";
+//   }
 
-  for( let j = 1; j <= i; j++){
-    line += j;
-  }
+//   for( let j = 1; j <= i; j++){
+//     line += j;
+//   }
 
-  for (let j = i - 1; j >= 1; j--){
-    line += j;
-  }
+//   for (let j = i - 1; j >= 1; j--){
+//     line += j;
+//   }
 
-  console.log(line);
+//   console.log(line);
 
-}
+// }
+
