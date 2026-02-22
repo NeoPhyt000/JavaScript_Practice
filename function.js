2896afabea755606a1371a5df61d4dae15bc0d05
Dibosh Sarker <diboshsarker45@gmail.com>
2026-02-22 23:37:10 +0600

    first class function

---
+++ function.js
@@ -0,0 +1,42 @@
+// Function statement
+// function dibosh(){
+//     console.log("Kaise ho mere bhai");
+// }
+// dibosh();
+
+// Function expression
+// let fnc = function(){
+//}
+
+// Fat arrow function
+// let fnc = ()=>{
+//    console.log("heheheheh");
+// }
+// fnc();
+
+// function dance(d1){
+//     console.log(`${d1} naach raha hai`);
+// }
+
+// dance("ghoda");
+// dance("hati");
+// dance("kutta");
+
+// function add(a1 = 0, a2 = 0){
+//     console.log(a1 + a2);
+// }
+
+// add(1, 2);
+// add(3, 4);
+
+// function dibosh(...val){
+//     console.log(val);
+// }
+// dibosh(1,2,3,4,5,6,7,8,9);
+
+function bnp (){
+    return 20;
+}
+
+let val = bnp();
+console.log(val);
